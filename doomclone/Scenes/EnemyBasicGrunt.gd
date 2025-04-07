7b407d92d3e2fe4c5f0987dbc3da6db7997539b5
HopperAlex04 <hopperalex04@gmail.com>
2025-04-06 21:00:36 -0400

    Updated Seeker AI patterns to delay attacks, next task, implement projectiles and tracking.

--- doomclone/Scenes/EnemyBasicGrunt.gd
+++ doomclone/Scenes/EnemyBasicGrunt.gd
@@ -5,6 +5,9 @@ extends CharacterBody3D
 var speed = 5
 var health = 20
 
+func _ready() -> void:
+	$Timer.start()
+
 func _physics_process(_delta):
 	var current_location = global_transform.origin
 	var next_location = nav.get_next_path_position()
@@ -12,17 +15,22 @@ func _physics_process(_delta):
 	look_at(nav.target_position)
 	if is_on_floor() or new_velocity.y <= -4:
 		new_velocity.y = -.1
-	print(new_velocity.y)
+	#print(new_velocity.y)
 	set_velocity(new_velocity)
 	$AnimatedSprite3D.play("Walk")
 	move_and_slide()
 
 func update_target_location(target_location):
-	if global_transform.origin.distance_to(target_location) > 15:
+	if global_transform.origin.distance_to(nav.target_position) > 15:
+		set_process(true)
+		set_physics_process(true)
 		nav.target_position = target_location
-		print(nav.distance_to_target())
 	else:
-		shoot()
+		set_process(false)
+		set_physics_process(false)
+		nav.target_position = target_location
+	
+		#print(nav.distance_to_target())
 	
 func take_damage(dmg_amount):
 	health -= dmg_amount
@@ -41,9 +49,23 @@ func death():
 		$AnimatedSprite3D.play("Die")
 	
 func shoot():
+	print("freshCall")
 	set_process(false)
 	set_physics_process(false)
 	$AnimatedSprite3D.play("shoot")
-	await $AnimatedSprite3D.animation_finished
+	#await $AnimatedSprite3D.animation_finished
+	#$Timer.start()
+	#set_process(true)
+	#set_physics_process(true)
+
+
+func _on_timer_timeout() -> void:
+	#print("timeout")
+	if global_transform.origin.distance_to(nav.target_position) < 15:
+		shoot()
+
+
+func _on_animated_sprite_3d_animation_finished() -> void:
+	#print("timeout")
 	set_process(true)
-	set_physics_process(true)
+	set_physics_process(true) # Replace with function body.
